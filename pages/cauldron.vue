6986c5962cb61d45af0c9549f6f53387d31987af
dipwave <dipwave@users.noreply.github.com>
2025-12-04 14:02:55 +0300

    fix(cauldron): update instruction text from "below" to "above"
    - Change user instruction text in control panel from "type below" to "type above"
    - Corrects misleading UI guidance to match actual input field position
    - Improves user experience by providing accurate directional reference

--- pages/cauldron.vue
+++ pages/cauldron.vue
@@ -105,7 +105,7 @@
             <span v-else-if="ingredients.length > 0"
               >{{ 3 - ingredients.length }} more to converge</span
             >
-            <span v-else>Drag cards or type below</span>
+            <span v-else>Drag cards or type above</span>
           </p>
         </div>
 
